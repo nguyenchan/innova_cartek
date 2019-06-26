5d49914b3c5d1de27c25c05697ad576e17504ca1
pkcnguyen@gmail.com <pkcnguyen@gmail.com>
2019-06-26 11:52:23 +0700

    add screen test

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,21 +1,60 @@
 import React from "react";
 import { ScrollView, StyleSheet, Text } from "react-native";
-// import { ExpoLinksView } from '@expo/samples';
+import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
+import { ApolloProvider, graphql } from "react-apollo";
+import gql from "graphql-tag";
+
+const carQuery = gql`
+	query {
+		ymmes(db_market: "US") {
+			text
+			enum
+		}
+	}
+`;
+
+const CarComponent = graphql(carQuery)(props => {
+	const { error, ymmes } = props.data;
+	console.log(props);
+	if (error) {
+		return <Text>{error}</Text>;
+	}
+	if (ymmes) {
+		return (
+			<ScrollView>
+				{ymmes.map(ymmes => {
+					return <Text key={ymmes.text}>{ymmes.text}</Text>;
+				})}
+			</ScrollView>
+		);
+	}
+	return <Text>loading...</Text>;
+});
+
+const client = new ApolloClient({
+	link: new HttpLink({
+		uri: "http://34.210.160.172:4000/graphql",
+		headers: {
+			// authorization: "YOUR_TOKEN" // on production you need to store token in storage or in redux persist, for demonstration purposes we do this like that
+		}
+	}),
+	cache: new InMemoryCache()
+});
 
 export default function LinksScreen() {
 	return (
 		<ScrollView style={styles.container}>
-			{/**
-			 * Go ahead and delete ExpoLinksView and replace it with your content;
-			 * we just wanted to provide you with some helpful links.
-			 */}
-			<Text>some link</Text>
+			<Text>graphql query</Text>
+			<ApolloProvider client={client}>
+				<Text style={styles.welcome}>Result bellow:</Text>
+				<CarComponent />
+			</ApolloProvider>
 		</ScrollView>
 	);
 }
 
 LinksScreen.navigationOptions = {
-	title: "Links"
+	title: "Year"
 };
 
 const styles = StyleSheet.create({
